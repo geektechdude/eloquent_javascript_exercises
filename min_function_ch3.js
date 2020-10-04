90621e0e488b07605e063fab4831103c9024d8a3
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2020-10-04 14:23:58 +0100

    adding ch3 min_function exercise

---
+++ min_function_ch3.js
@@ -0,0 +1,9 @@
+// function to take 2 values and return the minimum of the two
+function minimum(x, y) {
+    if (x < y){
+        return(x)
+    }
+    else if (y < x) {
+        return(y)
+    }
+}
